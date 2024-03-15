96381f6bff0d75ca1603c02f47dcffc8378e369b
Atriel1999 <fortuny1999@gmail.com>
2024-03-15 14:40:55 +0900

    양병학 3/15 오후 2:40 (html 2개수정 2개추가, css1개수정 2개추가, js파일 1개추가, 이미지파일추가) myimage 이미지 추가 (ybh 폴더) 모임 사진첩 관련 js 파일 추가(gallery.js) 문화재추천, 문화재행사검색 css파일 추가(heritage-recommend,heritage-search.css)
    문화재 행사 검색, 문화재 행사 상세페이지 수정 (heratiage-event-search,single.html)
    문화재 추천, 모임 사진첩 추가(heritage-recommend.html, club-photo-album.html)

---
+++ FinalProject/src/main/resources/static/js/gallery.js
@@ -0,0 +1,35 @@
+// 사진첩 관련 코드
+var gallery = document.querySelector('#gathering-gallery');
+var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+var resizeAll = function () {
+    var altura = getVal(gallery, 'grid-auto-rows');
+    var gap = getVal(gallery, 'grid-row-gap');
+    gallery.querySelectorAll('.gathering-gallery-item').forEach(function (item) {
+        var el = item;
+        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    });
+};
+gallery.querySelectorAll('img').forEach(function (item) {
+    item.classList.add('byebye');
+    if (item.complete) {
+        console.log(item.src);
+    }
+    else {
+        item.addEventListener('load', function () {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            var gitem = item.parentElement.parentElement;
+            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+            item.classList.remove('byebye');
+        });
+    }
+});
+//자동 크기 조절, 클릭시 보여주기
+window.addEventListener('resize', resizeAll);
+gallery.querySelectorAll('.gathering-gallery-item').forEach(function (item) {
+    item.addEventListener('click', function () {        
+        item.classList.toggle('full');        
+    });
+});
+//사진첩 코드 끝
