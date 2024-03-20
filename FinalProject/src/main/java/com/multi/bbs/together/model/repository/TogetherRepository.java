9cdb71b82940d9cc26cda02df3fe63e33eeae953
MinJun Park <zoshs38@gmail.com>
2024-03-20 11:07:37 +0900

    home모임

---
+++ FinalProject/src/main/java/com/multi/bbs/together/model/repository/TogetherRepository.java
@@ -0,0 +1,12 @@
+//package com.multi.bbs.together.model.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.multi.bbs.member.model.vo.Member;
+//import java.util.List;
+//
+//
+//public interface TogetherRepository extends JpaRepository<Member, Integer> { 
+//	Member findByMemberId(String memberId);
+//	Member findByKakaotoken(String kakaotoken);
+//}
---
+++ FinalProject/src/main/java/com/multi/bbs/together/model/service/TogetherRepository.java
@@ -0,0 +1,12 @@
+//package com.multi.bbs.together.model.service;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import com.multi.bbs.member.model.vo.Member;
+//import java.util.List;
+//
+//
+//public interface TogetherRepository extends JpaRepository<Member, Integer> { 
+//	Member findByMemberId(String memberId);
+//	Member findByKakaotoken(String kakaotoken);
+//}
